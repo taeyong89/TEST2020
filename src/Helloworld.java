e7de2fabc1380a80fcf6bafb2508fe01dcebb832
korea <korea@DESKTOP-OCCAE4O.mshome.net>
2020-11-01 16:01:40 +0900

    "열심히 하면 개발자가 될 수 있습니다." 메시지 추가

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -3,5 +3,6 @@ public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요, 자바개발자 여러분, 환영합니다");
 		System.out.println("열심히 개발에 집중해주세요");
+		System.out.println("열심히 하면 개발자가 될 수 있습니다.");
 	}
 }
